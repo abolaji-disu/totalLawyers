47e09c7df210a694ae6de6afd2991560bb499677
Abolaji Disu <abolajiidiisu@gmail.com>
2024-08-22 02:34:38 +0100

    completed last page [all page web view done]

--- pages/page5.js
+++ pages/page5.js
@@ -1,5 +1,50 @@
 export default `
-
-
+<div class="last-page">
+    <p>We found 1 SSDI lawyer in your area</p>
+    
+     <label>
+        First Name
+        <div>
+        <input type="text" name="First Name" placeholder="Enter First Name" />
+        </div>
+    </label>
+    
+     <label>
+        Last Name
+        <div>
+        <input type="text" name="Last Name" placeholder="Enter Last Name" />
+        </div>
+    </label>
+    
+     <label>
+        Phone Number
+        <div>
+        <input type="text" name="Phone Number" placeholder="Enter Phone Number" />
+        </div>
+    </label>
+    
+    
+     <label>
+        Email
+        <div>
+        <input type="text" name="Email" placeholder="Enter Email Address" />
+        </div>
+    </label>
+    
+        <button class="last-step">Submit</button>    
+        <p class="base-paragraph">
+      By submitting this form I agree to the  <a href="https://www.internetbrands.com/ibterms/" target="_blank">Terms of Use<a> and
+        <a href="https://www.internetbrands.com/privacy/privacy-main/" target="_blank">Privacy Policy </a> and 
+       consent to be contacted  by Martindale-Nolo and its affiliates, and up to three attorneys regarding this request and to receiving relevant
+      marketing messages by automated means, text and/or prerecorded messages at the number provided. Consent is not required 
+      as a condition of service, <a href="https://www.figma.com/exit?url=javascript%3Avoid(0)" target="_blank">Click Here<a> 
+      to agree without providing consent to be contacted by automated means, text and/or prerecorded messages. Rates may apply.
+      <br /><br />
+      You should not send any sensitive or confidential information through this site. Any information sent through this 
+      site does not create an attorney-client relationship and may not be treated as privileged or confidential. 
+      The lawyer or law firm you are contacting is not required to, and may choose not to, accept you as a client. 
+      The Internet is not necessarily secure and emails sent through this site could be intercepted or read by third parties.  
+      </p>
+</div>
 
 `
